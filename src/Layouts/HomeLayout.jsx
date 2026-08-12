8c7263a00622b501a50e184f4b77e5a7e8c355b2
Ayaz Ahmed <ayazahmed11257@gmail.com>
2026-08-12 19:41:20 +0530

    added login signup button to sidebar

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -77,13 +77,29 @@
 
 
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FiMenu } from "react-icons/fi";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from '../Components/Footer'; 
 
 function HomeLayout({ children }) {
   const [open, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  //for checking if user is logged in
+  const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+  // for displaying options according to the role
+  const role = useSelector((state) => state?.auth?.role);
+
+  function handleLogout(e){
+    e.preventDefault();
+    // const res = await dispatch(logout());
+    // if(res?.payload?.success)
+    navigate('/')
+  }
 
   return (
     <div className="relative">
@@ -119,6 +135,11 @@ function HomeLayout({ children }) {
               </button>
             </li>
             
+            {isLoggedIn && role === 'ADMIN' && (
+              <li>
+                <Link to='/admin/dashboard'>Admin Dashboard</Link>
+              </li>
+            )}
 
             <li className="w-full">
               <Link to="/courses">All Courses</Link>
@@ -129,6 +150,31 @@ function HomeLayout({ children }) {
             <li className="w-full">
               <Link to="/about">About Us</Link>
             </li>
+
+            {!isLoggedIn && (
+             
+              <div className="w-full flex items-center justify-center space-x-1">
+                <button className="btn-primary bg-orange-600 px-4 py-1 font-semibold rounded-md w-full hover:bg-orange-800">
+                  <Link to='/login'>Login</Link>
+                </button>
+                <button className="btn-secondary bg-orange-600 px-3 py-1 font-semibold rounded-md w-full hover:bg-orange-800">
+                  <Link to='/login'>Signup</Link>
+                </button>
+              </div>
+              
+            )}
+            {isLoggedIn && (
+              <li className="absolute bottom-4 w-[90%]">
+              <div className="w-full flex items-center justify-center space-x-1">
+                <button className="btn-primary bg-orange-600 px-4 py-1 font-semibold rounded-md w-full hover:bg-orange-800">
+                  <Link to='/user/profile'>Profile</Link>
+                </button>
+                <button className="btn-secondary bg-orange-600 px-3 py-1 font-semibold rounded-md w-full hover:bg-orange-800">
+                  <Link onClick={handleLogout}>Logout</Link>
+                </button>
+              </div>
+              </li>
+            )}
           </ul>
         </div>
       )}

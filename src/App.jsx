b5b0ee3eae8c91f8a03a20606a3d9e0dcaf557f2
Ayaz Ahmed <ayazahmed11257@gmail.com>
2026-08-23 12:03:35 +0530

    Course description component added

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import Login from './Pages/Login.jsx';
 import CourseList from './Pages/Course/CourseList.jsx';
 import Contact from './Pages/Contact.jsx';
 import Denied from './Pages/Denied.jsx';
+import CourseDescription from './Pages/Course/CourseDescription.jsx';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route path="/signup" element={<Signup />} ></Route>
           <Route path="/login" element={<Login />} ></Route>
           <Route path="/courses" element={<CourseList />} ></Route>
+          <Route path="/course/description" element={<CourseDescription />} ></Route>
           <Route path="/contact" element={<Contact />} ></Route>
           <Route path="/denied" element={<Denied />} ></Route>
            <Route path="*" element={<NotFound />} ></Route> 

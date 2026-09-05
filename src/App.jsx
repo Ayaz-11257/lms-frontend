3972c6afaea0acd81d1aa4ebf357cdafbd9dfc57
Ayaz Ahmed <ayazahmed11257@gmail.com>
2026-09-05 23:42:25 +0530

    added edit profilepage successfully

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import CourseDescription from './Pages/Course/CourseDescription.jsx';
 import RequireAuth from './Components/Auth/RequireAuth.jsx';
 import CreateCourse from './Pages/Course/CreateCourse.jsx';
 import Profile from './Pages/User/Profile.jsx';
+import EditProfile from './Pages/User/EditProfile.jsx';
 
 
 function App() {
@@ -44,6 +45,7 @@ function App() {
 
           <Route element={<RequireAuth allowedRoles={["ADMIN", "USER"]} />}>
           <Route path="/user/profile" element={<Profile />} ></Route>
+          <Route path="/user/editprofile" element={<EditProfile />} ></Route>
           </Route>
         </Routes>
     </>

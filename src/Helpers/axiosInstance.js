36bb98b3a6956a5085deece226a7ae28cb5aeb80
Ayaz Ahmed <ayazahmed11257@gmail.com>
2026-09-15 11:56:09 +0530

    updated base url

--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5001/api/v1";
+// const BASE_URL = "http://localhost:5001/api/v1";
+const BASE_URL = "https://lms-backend-y596.onrender.com/api/v1";
 
-const axiosInstance = axios.create();
+
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
 
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
